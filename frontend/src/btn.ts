91f378ca74d2122295ce2bada7db420112932e1d
lincot <lincot@disroot.org>
2025-12-18 20:29:27 +0300

    improve button styling

--- frontend/src/btn.ts
+++ frontend/src/btn.ts
@@ -1,6 +1,6 @@
 export const btn = (enabled: boolean) =>
   `px-4 py-2 rounded-lg text-white ${
     enabled
-      ? "dark:bg-neutral-900 hover:bg-neutral-800"
+      ? "bg-neutral-900 dark:bg-neutral-900 border dark:border-gray-300 hover:bg-neutral-700 dark:hover:bg-neutral-800"
       : "bg-gray-300 dark:bg-gray-500 cursor-not-allowed"
   }`;
